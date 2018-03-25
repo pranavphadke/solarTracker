20dfb33bb26cd73fa61d641047d12d6b27689e46
Pranav Phadke <pranavphadke@yahoo.com>
2018-03-25 13:56:01 -0500

    position controller initial design

--- src/motor_model_daq/plotter.m
+++ src/motor_model_daq/plotter.m
@@ -6,7 +6,12 @@ clc
 load dataOut_motor_w_gb.txt
 plot(dataOut_motor_w_gb(:,1),dataOut_motor_w_gb(:,2))
 hold
-G=tf(15.5,[1,5.95])
+G=tf(15.235,[1,5.95])
 step(255*G)
 hold off
 sisotool(G)
+
+figure()
+load dataOut_motor_wo_gb.txt
+plot(dataOut_motor_wo_gb(:,1),dataOut_motor_wo_gb(:,2))
+% motor encoder pulse to shaft angle =0.3 * pulse
